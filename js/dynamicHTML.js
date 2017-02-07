a34b2820262733a2d6c4e1b0cf0aedccfa005342
Leon Do <leondo123@gmail.com>
2017-02-07 17:10:41 -0500

    added input section

--- js/dynamicHTML.js
+++ js/dynamicHTML.js
@@ -1,25 +1,24 @@
 // this file is used to dynamically build the website
 
+var coinArray = ['USD', 'BTC', 'ETC', 'XMR', 'ETH', 'DASH', 'LTC', 'ZEC', 'NXT', 'XRP', 'REP', 'STR']
 
 //radio buttons
-$.get('http://www.coincap.io/coins').done(function(allData){
-	for (var i = 0; i < allData.length; i++){
+for (var i = 0; i < coinArray.length; i++){
 
-		//radio button column 1
-		$('.form1').append(` 
-		    <p>
-		      <input name="group1" type="radio" id="${allData[i]}1" data-name="${allData[i]}" />
-		      <label for="${allData[i]}1">${allData[i]}</label>
-		    </p>
-		`)
+	//radio button column 1
+	$('.form1').append(` 
+	    <p>
+	      <input name="group1" type="radio" id="${coinArray[i]}1" data-name="${coinArray[i]}" />
+	      <label for="${coinArray[i]}1">${coinArray[i]}</label>
+	    </p>
+	`)
 
-		//radio button column 2
-		$('.form2').append(` 
-		    <p>
-		      <input name="group2" type="radio" id="${allData[i]}2" data-name="${allData[i]}" />
-		      <label for="${allData[i]}2">${allData[i]}</label>
-		    </p>
-		`)
-	}
-})
+	//radio button column 2
+	$('.form2').append(` 
+	    <p>
+	      <input name="group2" type="radio" id="${coinArray[i]}2" data-name="${coinArray[i]}" />
+	      <label for="${coinArray[i]}2">${coinArray[i]}</label>
+	    </p>
+	`)
+}
 
