2670c000cbccd88be3a3a1258dc4cace8f3315d5
Leon Do <leondo123@gmail.com>
2017-02-07 10:28:30 -0500

    hard coded coin names

--- js/dynamicHTML.js
+++ js/dynamicHTML.js
@@ -1,25 +1,24 @@
 // this file is used to dynamically build the website
 
+var coinArray = ["BTC","LTC","ETC","NXT","DASH","1ST","ZEC","TRIG","ZCL","PLU","VRS","SBD","ANS","TP1","AEON","ARDR","BABCOIN","SLK","XZC","ESP2","BIC","PLU","SILK","REP","SNGLS","OMNI","1CR","BLOCKPAY","STRAT","PEERPLAYS","LEO","ETH","DAO","EXP","PPC","DOGE","FCT","LSK","BRK","LBC","WAVES","INFX","DGD","DCR","RISE","PLS","BKS","CRBIT","SIB","YOC","OBITS","XVG","SC","CREVA","VOX","STEEM","SCOT","SEC","XVC","RADS","DCR","MUSE","AGRS","AMP","XLM","NMC","XRP","QRK","DGC","BTCD","LXC","XMR","XCP","ACCI","FAIL","BTA","FIC","LFO","TAGR","XC","AUR","PTS","BC","DVC","MINT","MGW","NAS","SFR","CREDIT","ZET","VIA","FTC","XPM","VRC","LTS","GML","IFC","VTC","XCR","QORA","MAX","ZCC","GAY","WDC","YBC","NXTTY","FRSH","XCN","BTB","PRT","TIX","RED","NET","EXC","CANN","BCN","MSC","MONA","STR","RDD","NVC","XDN","POT","SYS","ONEC","IXC","SYNC","JLH","BILS","ANC","SDC","KARM","PND","NOTE","BBR","RZR","RZR","URO","UNO","NAUT","GLD","HYPER","NLG","XDP","DGB","JPC","NBT","ESC","NODE","UNITY","EXCL","XTC","MAID","BURST","DEM","SWIFT","MRKT","SHIBE","UIS","ICASH","LUX","CRT","CAM","UNAT","EAGS","P7C","CF","CFN","URC","BFX","COINO","PRO","10K","SF0","NXTCOINSCO","NXTPRIVACY","SLR","MTC","ATOMIC","CINNI","ISR","PRIVATEBET","AC","CCS","WATER","UCI","XWC","FLT","NOBL","XAP","CCN","CAP","RIN","TRL","XCPSCO","NTR","XCH","PLUGIN","MMC","VTA","METAL","CESC","TES","GAIA","NXTI","PHS","CZC","OC","MWC","FLDC","QBK","BCX","GRS","FST","COMM","LTCD","DOPE","XWT","NOXT","XPD","MTR","FCN","DORCS","RBBT","DRKC","ICB","MRC2","SCSY","MNE","CRAIG","THC","APEX","TOP","XLB","WETK","SPA","NRS","HVC","NOO","PIGGY","BUK","LKY","LGD","PSEUD","SMC","TOR","MMNXT","XAU","TRUST","RBR","TEK","XBS","JBS","EAC","APC","ECC","UTC","CACH","MRCAM","FIBRE","HBN","XST","FLO","XAI","VPN","MZC","MOON","BOST","RBY","OPAL","BLITZ","MARYJ","FRC","AMBER","BTM","USDE","DEBUNE","BITS","XSI","HTML5","HYP","MYR","XMG","UNC","BLU","ABY","EKN","PINK","TRC","FLAP","SPR","CBX","UNB","C2","AM","BITB","NXS","CRYPT","BYC","LTBC","NEOS","RIC","NTRN","GEO","COL","TAG","EFL","LEAF","CASH","GAME","MED","BTCS","ELT","DARK","8BIT","OMC","SXC","DIME","TIPS","SUPER","EMC2","XBC","KORE","CSC","SMAC","MMXIV","J","PTC","SAT2","NKA","XXX","UTIL","HUC","SLM","PLNC","XCASH","ASC","CHA","U","GCN","GB","CAIX","QSLV","WKC","CKC","POP","EXE","GIVE","HAM","PXC","CAT","NYC","MEOW","VIOR","KDC","DIEM","GUE","KGC","SBC","OCTO","HRNXT","BUN","QCN","BET","VDO","MRY","GLYPH","SHLD","CDN","ALN","TRI","GSM","FFC","SHADE","BLZ","VCOIN","BVC","SHA","NMB","XBOT","BRX","VGC","CND","ICN","STK","MCN","CRACK","SPT","HRL","CAGE","YUM","ZS","PYC","ULTC","STV","SOLE","GAP","LTB","BNCR","RIPO","OPALTKN","CNL","GIZ","MLS","JKC","GP","TCO","CATC","PLC2","NRB","BEL","QB","DSB","PMP","BEAR","NTC","ORO","GUN","PHO","BOB","NEC","ADN","BEN","AXR","DTC","DASH","BTS","BANX","XPY","NSR","SWARM","XEM","ARCH","RBT","GEMZ","DEX","BLK","FAIR","FC2","CELL","CLAM","START","SJCX","PANGEA","NXTV","JINN","SKYNET","I0C","GRC","NAV","BLOCK","USDT","LIQUID","ZEIT","CRAVE","BAY","CURE","MEC","JAY","EMC","TRON","BITUSD","ZRC","VNL","FIMK","HZ","CNMT","DICE","BITCNY","DMD","SMLY","DOGED","IOC","QTL","SONG","ARI","SLG","ORB","XDQ","VOOT","SRC","CYP","BLC","BITGOLD","BQC","YAC","MUE","LDOGE","BITBTC","MIN","SSD","XQN","WBB","CLR","COOL","NYAN","BSTY","KTK","CTM","CON","NKT","EGMA","DRZ","UFO","499","SIGU","ACOIN","SMBR","KEY","ELC","FRK","LOT","ENRG","KUMA","ROOT","GDC","CRW","LIMX","TIT","CIN","EMD","GLC","MNC","BTG","JUDGE","CXC","LOG","KOBO","XJO","OK","LSD","FRAC","HLC","RPC","HAL","ASN","DBL","SOON","LYC","BRIT","ZED","IEC","COV","XGR","DOX","ARG","MRS","TRK","GSX","CHASH","BTQ","007","GHC","GRE","GLIDE","BTCRY","TAK","BITZ","EVENT","ANAL","CGA","CC","VMC","42","BOOM","AU","NXTSCO","CZR","SLING","TWIST","GUA","SOL","BAT","XPL","XDB","MAC","PXI","XCO","CORG","HEX","RT2","HIRO","MAIA","KRN","CCC","488","BTX","SPHR","OSC","XMS","ROS","CHILD","PFC","DP","XTP","CYC","BCY","KR","RBIES","EUC","ADZ","ION"]
+
 
 //radio buttons
-$.get('https://www.coincap.io/coins').done(function(allData){
-	for (var i = 0; i < allData.length; i++){
+for (var i = 0; i < coinArray.length; i++){
 
-		//radio button column 1
-		$('.form1').append(` 
-		    <p>
-		      <input name="group1" type="radio" id="${allData[i]}1" data-name=${allData[i]} />
-		      <label for="${allData[i]}1">${allData[i]}</label>
-		    </p>
-		`)
+	//radio button column 1
+	$('.form1').append(` 
+	    <p>
+	      <input name="group1" type="radio" id="${coinArray[i]}1" data-name=${coinArray[i]} />
+	      <label for="${coinArray[i]}1">${coinArray[i]}</label>
+	    </p>
+	`)
 
-		//radio button column 2
-		$('.form2').append(` 
-		    <p>
-		      <input name="group2" type="radio" id="${allData[i]}2" data-name=${allData[i]} />
-		      <label for="${allData[i]}2">${allData[i]}</label>
-		    </p>
-		`)
-	}
-});
-
+	//radio button column 2
+	$('.form2').append(` 
+	    <p>
+	      <input name="group2" type="radio" id="${coinArray[i]}2" data-name=${coinArray[i]} />
+	      <label for="${coinArray[i]}2">${coinArray[i]}</label>
+	    </p>
+	`)
+}
