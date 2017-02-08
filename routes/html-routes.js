50f9d32e2589dfc390ac9efa0089a2ad18f195da
morgansliman <morgansliman@gmail.com>
2017-02-08 00:14:49 -0500

    Added Express server and MVC file structure

---
+++ routes/html-routes.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = (app) => {
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+	app.get('/login', (req, res) => {
+		res.sendFile(path.join(__dirname + '/../public/login.html'));
+	});
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+//  Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//  Setting up the express app
+const app = express();
+const PORT = 8000;
+
+//  Sets up express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//  Static directory
+app.use(express.static('./public'));
+
+//  Routes
+require('./routes/html-routes')(app);
+require('./routes/api-routes')(app);
+
+//  Starting the express app
+app.listen(PORT, () => {
+	console.log('Express listening on port:', PORT);
+});
