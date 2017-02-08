7d946c6e1f040ff78a56810db6b16690cf464dd4
Leon Do <leondo123@gmail.com>
2017-02-08 14:53:53 -0500

    added coincap back

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -52,28 +52,37 @@ function callAPI(crypto1, crypto2, transferAmount){
 
 /*
 
-		function callAPI(crypto1, crypto2){
-			$.get('https://www.coincap.io/front').done(function(allData){
+function callAPI(crypto1, crypto2){
+	$.get('https://www.coincap.io/front').done(function(allData){
 
-				// find price of BTC and ETC
-				for (var i = 0; i < allData.length; i++){
+		// find price of BTC and ETC
+		for (var i = 0; i < allData.length; i++){
 
-					// if(BTC === BTC)
-					if (allData[i].short === crypto1.name){
-						//add to price to object
-						crypto1.price = allData[i].price
-					}
+			// if(BTC === BTC)
+			if (allData[i].short === crypto1.name){
+				//add to price to object
+				crypto1.price = allData[i].price
+			}
 
-					if (allData[i].short === crypto2.name){
-						crypto2.price = allData[i].price
-					}
+			if (allData[i].short === crypto2.name){
+				crypto2.price = allData[i].price
+			}
 
-				}//for loop
+		}//for loop
 
-				cryptoConvert(crypto1, crypto2)
+		cryptoConvert(crypto1, crypto2)
 
-			})//$get
-		}//callAPI
+	})//$get
+}//callAPI
+
+
+function cryptoConvert(crypto1, crypto2){
+
+	// this 500 will be taken from the database
+	var userMoney = (500 * crypto1.price) / (crypto2.price)
+
+	alert(`Converting 500 ${crypto1.name} to ${crypto2.name} will give you ${userMoney} of ${crypto2.name}`)
+}
 
 
 
