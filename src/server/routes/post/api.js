800b145181bd1da0d0a8c40855757a31ce3046e5
chasejthompson <chasethompson@Chases-MacBook-Pro.local>
2017-02-13 18:49:42 -0500

    posting updated amounts, we'll do logic beforehand

--- src/server/routes/post/api.js
+++ src/server/routes/post/api.js
@@ -6,8 +6,7 @@ const Transaction = require('../models/Transaction.js');
 
 //update wallet when transfer button is clicked
 // =============================================================
-router.post('/:username/:coin1/:coin2/wallet/transact', function(req, res) {
-    let entry = {};
+router.post('/:username/wallet/transact', function(req, res) {
     Transaction.create({
         id: req.body.id,
         username: req.body.username,
@@ -23,8 +22,6 @@ router.post('/:username/:coin1/:coin2/wallet/transact', function(req, res) {
         doge: req.body.doge,
         zec: req.body.zec,
         lsk: req.body.lsk,
-        entry.coin1 = req.body.coin1,
-        entry.coin2 = req.body.coin2,
 
     }).then(function(data) {
         res.json(data);
