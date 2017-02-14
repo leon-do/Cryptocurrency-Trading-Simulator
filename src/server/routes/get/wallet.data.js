0e67a2e79639e57c92e6d3011dc83580936382fc
morgansliman <morgansliman@gmail.com>
2017-02-13 22:31:25 -0500

    staging for push

--- src/server/routes/get/wallet.data.js
+++ src/server/routes/get/wallet.data.js
@@ -1,12 +1,32 @@
+// Dependencies
+// =============================================================
+const express = require('express');
 const path = require('path');
+const router = express.Router();
+//express session
+
+// Models
+// =============================================================
+const User = require('../../models/User');
+const Transaction = require('../../models/Transaction');
+//the below is temporary
 const data = require('./data');
 
-module.exports = (app) => {
-	// temporary user parameter can currently be any word.
-	// We'll need to add a check for user authentication
-	app.get('/wallet', (req, res) => {
-		// this should make a query to the DB for user's last transaction data
-		// temporarily... it doesn't.
+//use /wallet by for testing until login works
+// =============================================================
+router.get('/wallet', (req, res) => {
 		res.json(data);
-	});
-};
+});
+
+router.get('/:username/wallet', (req, res) => {
+		Transaction.findOne({
+			where: {
+				username: req.params.username
+			},
+			order: [[ 'createdAt', 'DESC' ]]
+		}).then((data) => {
+			res.json(data);
+		});
+});
+
+module.exports = router;
